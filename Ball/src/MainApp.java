6a62c71d6085a0153717fe8a387e211ff28c3069
orosmatthew <orosmatthew@gmail.com>
2018-12-11 18:39:10 -0500

    Created a fixed loop that only loops 60 times per second. Fixes problem of faster framerate -> faster ball.

--- Ball/src/MainApp.java
+++ Ball/src/MainApp.java
@@ -19,22 +19,39 @@ public class MainApp extends JFrame{
        	SwingUtilities.updateComponentTreeUI(window);
         double x = 0;
         double y = 0;
+        //delta is milliseconds between each loop
+        int nanoDelta = 0;
+        long prevNano = 0;
+        double delta = 0;
+        boolean toggle = false;
         while(true){
-	        while(x<375){
-	        	x+=0.00001;
-	        	y+=0.00001;
-	        	blueBall.setBallX((int)x);
-	        	blueBall.setBallY((int)y);
-	        	window.repaint();
-
-	        }
-	        while(x>0){
-	        	x-=0.00001;
-	        	y-=0.00001;
-	        	blueBall.setBallX((int)x);
-	        	blueBall.setBallY((int)y);
-	        	window.repaint();
-	        }
+        	nanoDelta += System.nanoTime()-prevNano;
+        	if (nanoDelta>16666667) { //restricts loop to only loop every 1/60 of a second
+        		delta = nanoDelta/1000000.0;
+        		System.out.println(delta);
+        		prevNano = System.nanoTime();
+        		nanoDelta = 0;
+        		if(x>375 && toggle == false)
+        			toggle = true;
+        		else if(x<0 && toggle == true)
+        			toggle = false;
+		        if(toggle == false){
+		        	x+=0.01;
+		        	y+=0.01;
+		        	blueBall.setBallX((int)x);
+		        	blueBall.setBallY((int)y);
+		        	window.repaint();
+	
+		        }
+		        if(toggle == true){
+		        	x-=0.01;
+		        	y-=0.01;
+		        	blueBall.setBallX((int)x);
+		        	blueBall.setBallY((int)y);
+		        	window.repaint();
+		        }
+        	}
+	        
         }
     }
     public static void createWindow()
@@ -43,6 +60,8 @@ public class MainApp extends JFrame{
         window.setSize(406,428);
         window.setResizable(false);
         window.setVisible(true);
-        System.out.println(window.getContentPane().getSize());
+        //System.out.println(window.getContentPane().getSize());
+        long millis = System.currentTimeMillis();
+        System.out.println(millis);
     }
 }
