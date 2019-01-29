221d50a21ae24f1dfc0ea07c98336c986e31f054
Max <20morreale.michael@scsmustangs.org>
2019-01-29 15:27:47 -0500

    Added more to paddle

--- Ball/src/Paddle.java
+++ Ball/src/Paddle.java
@@ -3,58 +3,51 @@ import java.awt.Graphics;
 import java.awt.Point;
 import javax.swing.JComponent;
 public class Paddle {
-	private Color paddleFillColor;
+	private Color paddleFillColor = Color.BLACK;
 	private Color paddleBorderColor = Color.BLACK;
 	private double paddleX=0;
 	private double paddleY=0;
 	private int paddleWidth=0;
 	private int paddleHeight=0;
 	
+	public Paddle(double x,double y) {
+		paddleX=x;
+		paddleY=y;
+	}
 	public Color getFillColor() {
-	   return paddleFillColor;
+	    return paddleFillColor;
 	}
 	public void setpaddleFillColor(Color paddleFillColor) {
-	   this.paddleFillColor = paddleFillColor;
+	    this.paddleFillColor = paddleFillColor;
 	}
 	public Color getpaddleBorderColor() {
-	   return paddleBorderColor;
+	    return paddleBorderColor;
 	}
 	public void setpaddleBorderColor(Color paddleBorderColor) {
-	   this.paddleBorderColor = paddleBorderColor;
-	}
-	public double getX() {
-	   return paddleX;
-	}
-	public void setX(double paddleX) {
-	   this.paddleX = paddleX;
-	}
-	public double getY() {
-	   return paddleY;
-	}
-	public void setY(double paddleY) {
-	   this.paddleY = paddleY;
+	    this.paddleBorderColor = paddleBorderColor;
 	}
 	public int getWidth() {
-	   return paddleWidth;
+	    return paddleWidth;
 	}
 	public void setWidth(int paddleWidth) {
-	   this.paddleWidth = paddleWidth;
+	    this.paddleWidth = paddleWidth;
 	}
 	public int getHeight() {
-       return paddleHeight;
+        return paddleHeight;
 	}
 	public void setHeight(int paddleHeight) {
-		 this.paddleHeight = paddleHeight;
+		this.paddleHeight = paddleHeight;
+	}
+	public void move(int v) {
+		paddleY+=v;
 	}
 	private class MyComponent extends JComponent{
-
 		private static final long serialVersionUID = 1L;
-
 		public void paint(Graphics g){
-                g.setColor(Color.BLACK);
-                g.fillRect((int)getX(),(int)getY(), getHeight(),getWidth());
-                g.setColor(Color.BLACK);
-                g.drawRect((int)getX(),(int)getY(), getHeight(),getWidth());
+                g.setColor(getFillColor());
+                g.fillRect((int)paddleX,(int)paddleY, getHeight(),getWidth());
+                g.setColor(getpaddleBorderColor());
+                g.drawRect((int)paddleX,(int)paddleY, getHeight(),getWidth());
 		}
 	}
 }
