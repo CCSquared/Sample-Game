41976739af453ea3bb4191c0c5f9c41c88edfe6c
Gota7 <gotaseven@gmail.com>
2019-02-12 15:01:50 -0500

    Update Keys
    KEYYYYYYYYYYZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ

--- Ball/src/Keys.java
+++ Ball/src/Keys.java
@@ -1,22 +1,39 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 
 public class Keys {
 
+	private static HashMap<Integer, Boolean> keys;
+	private static HashMap<Integer, Boolean> keysPushed;
+	
 	public static void keyPressed(KeyEvent e) {
-		
+		keys.put(e.getKeyCode(), true);	
 	}
 
 	public static void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		keys.put(e.getKeyCode(), false);	
+		keysPushed.put(e.getKeyCode(), true);
 	}
 
 	public static void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
+	}
+	
+	public static boolean IsKeyDown(Integer keyCode) {
+		return keys.get(keyCode);
+	}
+	
+	public static boolean IsKeyUp(Integer keyCode) {
+		return !keys.get(keyCode);
+	}
+	
+	public static boolean IsKeyPushed(Integer keyCode) {
+		boolean pushed = keysPushed.get(keyCode);
+		keysPushed.put(keyCode, false);
+		return pushed;
 	}
 	
 }
